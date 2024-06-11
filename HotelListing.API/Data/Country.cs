9df30360704843852e094767b7b0f55ac156fae0
Thilina Weerasinghe <sachinthathilina@gmail.com>
2024-06-11 10:48:58 +0530

    Added Entity Framework and Created Database

---
+++ HotelListing.API/Data/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelListing.API.Data
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string ShortName { get; set; }
+
+        //methanadi eka country ekakta hotel kihipayk tyna nisa eka One to Many Relation eka thmai enne eka pahala wdiyta thmai implement krnn wenne
+        public virtual IList<Hotel> Hotels { get; set; }
+    }
+}
---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Sri Lanka",
+                    ShortName = "SL",
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "India",
+                    ShortName = "IN",
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Pakistan",
+                    ShortName = "PK",
+                }
+            );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Galadari",
+                    Address = "Colombo",
+                    CountryId = 1,
+                    Rating = 8.9
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Tag samudra",
+                    Address = "Hydrabad",
+                    CountryId = 2,
+                    Rating = 8.5
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Hilton",
+                    Address = "Lahore",
+                    CountryId = 3,
+                    Rating = 7.0
+                }
+            );       
+        }
+    }
+}
